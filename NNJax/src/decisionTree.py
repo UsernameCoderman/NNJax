604d4eaf7c5b3acdadd280d51f8945c76368db11
UsernameCoderman <andcgri@outlook.com>
2025-02-16 22:14:58 +0100

    Reqrite code to support XOR
    - adds examples from class
    - ADds example with comments
    
    - Started rewriting code to support XOR
    - Change generation of training data

---
+++ NNJax/src/decisionTree.py
@@ -0,0 +1,14 @@
+#meant to impliment a simple decsiion tree with jax to test, postponed due to sickness.
+
+import jax.numpy as jnp
+from jax import random
+improt array as arr
+
+array = jnp.array([1,1,2,3,4,1,3,2,1,1,2,3])
+
+
+
+def entropy(array):
+    length = array.len()
+
+
