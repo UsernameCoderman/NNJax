f89d07afb83d03505f71bf67684b4c3c5da4660c
UsernameCoderman <andcgri@outlook.com>
2025-03-14 00:15:08 +0100

    DatsetGenerator dynamic file name
    - Changes the filename from beeing needed to be named manually to be dynamic named based on the input

--- NNJax/src/dataSetGenerator.py
+++ NNJax/src/dataSetGenerator.py
@@ -4,15 +4,15 @@ import json
 
 
 
-BATCH_SIZE = 10000
-NUM_OF_BATCHES = 50
-BITS = 2
+BATCH_SIZE = 69
+NUM_OF_BATCHES = 69
+BITS = 8
 #Generates random pairs of 1 and 0 into an array based on batch size and number of batches, and has 2 possible values
 TRAINING_DATA = jnp.array(np.random.randint(2, size=(NUM_OF_BATCHES, BATCH_SIZE, BITS)))
 
 
 
-filename = "2Bit10kBatch50Samples.json"
+filename = f"{BITS}Bit{NUM_OF_BATCHES}Batch{BATCH_SIZE}Samples.json"
 # Convert JAX array to a list and save as JSON
 with open(filename, "w") as file:
     json.dump(TRAINING_DATA.tolist(), file)
