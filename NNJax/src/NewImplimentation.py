a6256a4742914a1676e1bb58cf1fef4697e0bd67
UsernameCoderman <andcgri@outlook.com>
2025-04-06 20:06:53 +0200

    Remove bias
    Reverted back to pre K fold
    Removed unused bias

--- NNJax/src/NewImplimentation.py
+++ NNJax/src/NewImplimentation.py
@@ -6,11 +6,9 @@ from jax import tree_util
 import json
 import re
 
-
-K_FOLD = 5
-NEURONS = 16
+NEURONS = 4
 #Filename for dataset
-filename = "8Bit100Batches1000SamplesPerBatch.json"
+filename = "4Bit3Batches2SamplesPerBatch.json"
 
 
 # Extract dataset properties from filename
@@ -20,6 +18,7 @@ if setUp:
     print("BITS:", BITS)
     print("NUM_OF_BATCHES:", NUM_OF_BATCHES)
     print("BATCH_SIZE:", BATCH_SIZE)
+    print("NERONS:", NEURONS)
 
 #Get data from file
 with open(filename, "r") as file:
@@ -28,6 +27,7 @@ TRAINING_DATA = jnp.array(data)
 
 # Generates the correct labels by checking if the sum is odd or even
 XOR_LABELS = (jnp.sum(TRAINING_DATA, axis=-1) % 2 != 0).astype(jnp.int32)
+
 # One hot encodes 1 to [1,0] and 0 to [0,1]
 LABELS = jax.nn.one_hot(XOR_LABELS, 2)
 
@@ -38,29 +38,31 @@ LABELS = jax.nn.one_hot(XOR_LABELS, 2)
 #
 # neural network with activation functions, for sigmoid
 def forward_pass(params, input_array):
-    hidden_layer = jax.nn.relu(jnp.dot(input_array, params["weight_hidden"]) + params["bias_hidden"])
-    output_layer = jnp.dot(hidden_layer, params["weight_output"]) + params["bias_output"]
+    hidden_layer = jax.nn.sigmoid(jnp.dot(input_array, params["weight_hidden"]))
+    output_layer = jnp.dot(hidden_layer, params["weight_output"])
     return output_layer
 
 
 # loss function, uses cross entropy to calulcate loss
 def loss(params, inputs, labels):
-    predicted = jax.nn.sigmoid(forward_pass(params, inputs))
-    loss_value = optax.sigmoid_binary_cross_entropy(predicted, labels)  # https://optax.readthedocs.io/en/latest/api/losses.html#optax.losses.sigmoid_binary_cross_entropy
+    predicted = forward_pass(params, inputs)
+    loss_value = optax.sigmoid_binary_cross_entropy(predicted,
+                                                    labels)  # https://optax.readthedocs.io/en/latest/api/losses.html#optax.losses.sigmoid_binary_cross_entropy
     return jnp.mean(loss_value)
 
 
 # training function,
-def train_model(training_data, labels, batch_size, traning_steps, epochs=10, learning_rate=0.8):
+def train_model(training_data, labels, traning_steps, epochs=10, learning_rate=0.1):
 
     params = {  # https://docs.jax.dev/en/latest/_autosummary/jax.numpy.array.html
         "weight_hidden": jnp.array(np.random.randn(BITS, NEURONS) * 0.1, dtype=jnp.float32),
         # https://numpy.org/doc/2.1/reference/random/generated/numpy.random.rand.html
-        "bias_hidden": jnp.array(np.zeros(NEURONS), dtype=jnp.float32),
         "weight_output": jnp.array(np.random.randn(NEURONS, 2) * 0.1, dtype=jnp.float32),
-        "bias_output": jnp.array(np.zeros(2), dtype=jnp.float32),
         # https://numpy.org/devdocs/reference/generated/numpy.zeros.html
     }
+    print("\nParams (full values):")
+    for name, value in params.items():
+        print(f"\n{name}:\n{value}")
 
     # print(f"{params}")
     optimizer = optax.adam(learning_rate)
@@ -70,43 +72,25 @@ def train_model(training_data, labels, batch_size, traning_steps, epochs=10, lea
     # step function
     @jax.jit
     def training_step(params, batch, labels, opt_state):
-        loss_value, grad = jax.value_and_grad(loss)(params, batch, labels)  # https://docs.jax.dev/en/latest/_autosummary/jax.value_and_grad.html
-        params_update, new_opt_state = optimizer.update(grad, opt_state, params)  # https://optax.readthedocs.io/en/latest/api/optimizers.html#adam
-        new_parameters = optax.apply_updates(params, params_update)  # https://optax.readthedocs.io/en/latest/api/apply_updates.html
+        loss_value, grad = jax.value_and_grad(loss)(params, batch,
+                                                    labels)  # https://docs.jax.dev/en/latest/_autosummary/jax.value_and_grad.html
+        params_update, new_opt_state = optimizer.update(grad, opt_state,
+                                                        params)  # https://optax.readthedocs.io/en/latest/api/optimizers.html#adam
+        new_parameters = optax.apply_updates(params,
+                                             params_update)  # https://optax.readthedocs.io/en/latest/api/apply_updates.html
         return new_parameters, new_opt_state, loss_value
 
     # training loop
     for epoch in range(epochs):
-        for i in range(0, len(training_data), batch_size):  # Iterate in batches
-            batch = training_data[i:i + batch_size]
-            current_labels = labels[i:i + batch_size]
-
+        for i in range(traning_steps):
+            batch = training_data[i]
+            current_labels = labels[i]
             params, opt_state, loss_value = training_step(params, batch, current_labels, opt_state)
         print(f"Epoch {epoch + 1}, Loss: {loss_value.item():.4f}")
 
-    return loss_value.item()
 
 
 
 
-fold_size = len(TRAINING_DATA)//K_FOLD
-fold_losses = []
 
-#divide dataset into K number of folds
-for fold in range(K_FOLD):
-    start_of_fold, end_of_fold = fold * fold_size, (fold+1) * fold_size
-    val_data, val_labels = TRAINING_DATA[start_of_fold:end_of_fold], LABELS[start_of_fold:end_of_fold]
-    train_data = jnp.concatenate([TRAINING_DATA[:start_of_fold], TRAINING_DATA[end_of_fold:]], axis=0)
-    train_labels = jnp.concatenate([LABELS[:start_of_fold], LABELS[end_of_fold:]], axis=0)
-    print(f"Training Fold {fold + 1}/{K_FOLD}")
-
-    fold_loss = train_model(train_data, train_labels, batch_size=BATCH_SIZE, traning_steps=len(train_data) // BATCH_SIZE)
-    fold_losses.append(fold_loss)
-
-
-average_kfold_loss = np.mean(fold_losses)
-print(f"Average K-Fold Loss: {average_kfold_loss:.4f}")
-
-
-
-
+train_model(TRAINING_DATA, LABELS, NUM_OF_BATCHES)
--- NNJax/src/dataSetGenerator.py
+++ NNJax/src/dataSetGenerator.py
@@ -4,13 +4,15 @@ import json
 
 
 
-BATCH_SIZE = 2
-NUM_OF_BATCHES = 3
-BITS = 4
+BATCH_SIZE = 1000
+NUM_OF_BATCHES = 89
+BITS = 3
 #Generates random pairs of 1 and 0 into an array based on batch size and number of batches, and has 2 possible values
 TRAINING_DATA = jnp.array(np.random.randint(2, size=(NUM_OF_BATCHES, BATCH_SIZE, BITS)))
 
-print(f"{TRAINING_DATA}")
+
+
+
 
 filename = f"{BITS}Bit{NUM_OF_BATCHES}Batches{BATCH_SIZE}SamplesPerBatch.json"
 # Convert JAX array to a list and save as JSON
